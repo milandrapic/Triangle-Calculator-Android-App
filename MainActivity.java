dfdad00e6a9b6a6b50da77c40d2614e0433e1599
milandrapic <55461949+milandrapic@users.noreply.github.com>
2019-11-07 14:55:03 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    //gets value from input field
     private double getValue(int id){
         View view = findViewById(id);
         EditText editText = (EditText) view;
@@ -24,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
         double in = Double.parseDouble(input);
         return in;
     }
-
+    //changes value of text field in application
     private void setValue(int id, String newValue){
         View view = findViewById(id);
         TextView textView = (TextView) view;
         textView.setText(newValue);
     }
-
+    //function that occurs when compute button is clicked on app
     public void compute(View view){
         double a = getValue(R.id.edgeA);
         double b = getValue(R.id.edgeB);
@@ -42,12 +42,17 @@ public class MainActivity extends AppCompatActivity {
         int i,j;
         i = 0;
         j = 0;
+        //array of all values
         double[] vals = {a,b,c,angA,angB,angC};
+        //counts how many nonzero values were entered
         while(i < 6){
             if(vals[i] <= 0)j++;i++;
         }
+        //if more than 3 nonzero values are entered then output error message
         if(j > 3) setValue(R.id.answer, "Error:\nnot enough values given");
+        //if a side value isnt given then output error message
         else if((a+b+c) <= 0) setValue(R.id.answer, "Error:\nmust enter at least one side");
+        //if there are no errors then continue with normal output
         else {
             Triangle t = TriangleFinder.calculate(a, b, c, angA, angB, angC);
             setValue(R.id.answer, t.printVE());
